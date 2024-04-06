3f8d7b895a98cbf765f5b0d2539a68e54823d7e9
samualhalder <samual.halder2020@gmail.com>
2024-04-07 00:13:09 +0530

    backend:connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log(process.env.PORT);
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("conneted to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("server started!!!");
 });
